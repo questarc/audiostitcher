2a197659bf40504c6666a819c544d70c91d3b8c1
questarc <questarc.tech@gmail.com>
2025-11-08 10:07:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import streamlit as st
-from PIL import Image
 
 st.set_page_config(page_title="Audio Stitcher Pro", page_icon="🎧", layout="wide")
 
-logo = Image.open("assets/logo.png")
-st.sidebar.image(logo, use_column_width=True)
 st.sidebar.title("Audio Stitcher Pro")
 st.sidebar.markdown("Navigate through the pages to upload, arrange, stitch, and download your audio files.")
 
